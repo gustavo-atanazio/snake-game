be935ddb676de60b645df28950c550d4bf48a9da
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-21 16:37:30 -0300

    Desenhando cenário

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ startButton.addEventListener("click", startGame);
 
 function startGame() {
     createCanvas();
+    drawGame();
+    // gameLoop();
 }
 
 function createCanvas() {
@@ -16,11 +18,44 @@ function createCanvas() {
 
     canvas = document.querySelector("canvas");
     canvas.innerText = "Seu navegador não suporta o jogo.";
-    canvas.width = "600";
-    canvas.height = "600";
+    canvas.width = "700";
+    canvas.height = "700";
     canvas.id = "game-screen";
     
     context = canvas.getContext("2d");
-    context.fillStyle = "#F00";
-    context.fillRect(10, 10, 10, 10);
+    // context.fillStyle = "#F00";
+    // context.fillRect(10, 10, 10, 10);
+}
+
+function drawGame() {
+    const colors = {
+        firstColor: "#54B435",
+        secondColor: "#82CD47"
+    };
+
+    drawLine(colors.firstColor, colors.secondColor, 0);
+    drawLine(colors.secondColor, colors.firstColor, 100);
+    drawLine(colors.firstColor, colors.secondColor, 200);
+    drawLine(colors.secondColor, colors.firstColor, 300);
+    drawLine(colors.firstColor, colors.secondColor, 400);
+    drawLine(colors.secondColor, colors.firstColor, 500);
+    drawLine(colors.firstColor, colors.secondColor, 600);
+}
+
+function gameLoop() {
+    setInterval(() => {
+        console.log("rodando");
+    }, 1000);
+}
+
+function drawLine(firstColor, secondColor, yPosition) {
+    for (let i = 0; i < 8; i++) {
+        context.fillStyle = firstColor;
+        context.fillRect(i * 200, yPosition, 100, 100);
+    }
+
+    for (let i = 0; i < 8; i++) {
+        context.fillStyle = secondColor;
+        context.fillRect((i * 200) + 100, yPosition, 100, 100);
+    }
 }
