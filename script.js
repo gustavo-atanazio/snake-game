d913aa0b62217c2489b93966fa3cef875608e7db
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-21 17:16:22 -0300

    Criando e movendo a cobra

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const startButton = document.querySelector(".start");
+
 let canvas;
 let context;
+
+const snake = [
+    {x: 200, y: 200},
+    {x: 250, y: 200},
+    {x: 300, y: 200}
+];
+
+const snakeColors = {
+    body: "#E55807",
+    head: "#F86F03"
+};
+
+const snakeSize = 50;
+let direction;
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
     createCanvas();
     drawGame();
-    // gameLoop();
+    gameLoop();
 }
 
 function createCanvas() {
@@ -40,12 +55,20 @@ function drawGame() {
     drawLine(colors.firstColor, colors.secondColor, 400);
     drawLine(colors.secondColor, colors.firstColor, 500);
     drawLine(colors.firstColor, colors.secondColor, 600);
+
+    drawSnake();
 }
 
 function gameLoop() {
     setInterval(() => {
         console.log("rodando");
-    }, 1000);
+
+        context.clearRect(0, 0, 700, 700);
+        
+        moveSnake();
+
+        drawGame();
+    }, 300);
 }
 
 function drawLine(firstColor, secondColor, yPosition) {
@@ -58,4 +81,40 @@ function drawLine(firstColor, secondColor, yPosition) {
         context.fillStyle = secondColor;
         context.fillRect((i * 200) + 100, yPosition, 100, 100);
     }
+}
+
+function moveSnake() {
+    if (!direction) return;
+
+    const head = snake[snake.length - 1];
+
+    if (direction === "right") {
+        snake.push({x: head.x + 50, y: head.y});
+    }
+
+    if (direction === "left") {
+        snake.push({x: head.x - 50, y: head.y});
+    }
+
+    if (direction === "down") {
+        snake.push({x: head.x, y: head.y + 50});
+    }
+
+    if (direction === "up") {
+        snake.push({x: head.x, y: head.y - 50});
+    }
+
+    snake.shift();
+}
+
+function drawSnake() {
+    context.fillStyle = snakeColors.body;
+
+    snake.forEach((position, index) => {
+        if (index === snake.length - 1) {
+            context.fillStyle = snakeColors.head;
+        }
+
+        context.fillRect(position.x, position.y, snakeSize, snakeSize);
+    })
 }
