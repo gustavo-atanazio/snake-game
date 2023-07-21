336d5f46ebcbba5c70a00bce101efe2cf0f1efae
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-21 12:41:11 -0300

    Dinamizando criação do canvas

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const canvas = document.querySelector("#game-screen");
-const context = canvas.getContext("2d");
+const startButton = document.querySelector(".start");
+let canvas;
+let context;
 
-context.fillStyle = "#F00";
-context.fillRect(10, 10, 10, 10);
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    createCanvas();
+}
+
+function createCanvas() {
+    const body = document.querySelector("body");
+    body.innerHTML = "";
+
+    body.appendChild(document.createElement("canvas"));
+
+    canvas = document.querySelector("canvas");
+    canvas.innerText = "Seu navegador não suporta o jogo.";
+    canvas.width = "600";
+    canvas.height = "600";
+    canvas.id = "game-screen";
+    
+    context = canvas.getContext("2d");
+    context.fillStyle = "#F00";
+    context.fillRect(10, 10, 10, 10);
+}
